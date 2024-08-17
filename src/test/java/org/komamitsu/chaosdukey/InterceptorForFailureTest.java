02515d21fc2f9dee2bcf27f5614078f1398e3a62
Mitsunori Komatsu <komamitsu@gmail.com>
2024-08-17 12:42:50 +0900

    Add InterceptorForFailureTest

---
+++ src/test/java/org/komamitsu/chaosdukey/InterceptorForFailureTest.java
@@ -0,0 +1,54 @@
+package org.komamitsu.chaosdukey;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.*;
+
+import java.io.IOException;
+import java.util.concurrent.Callable;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+class InterceptorForFailureTest {
+  @Mock Callable<?> callable;
+
+  @Test
+  void intercept_WithZeroPercentage_ShouldNotThrowException()
+      throws Exception {
+    InterceptorForFailure interceptor =
+        spy(
+            new InterceptorForFailure(
+                new Config(
+                    new Config.DelayConfig.Builder().build(),
+                    new Config.FailureConfig.Builder()
+                            .setPercentage(0)
+                            .build(),
+                    true)));
+    int n = 1000;
+    for (int i = 0; i < n; i++) {
+      interceptor.intercept(callable);
+    }
+    verify(callable, times(n)).call();
+  }
+
+  @Test
+  void intercept_WithOneHundredPercentage_ShouldAlwaysThrowException() throws Exception {
+    InterceptorForFailure interceptor =
+        spy(
+            new InterceptorForFailure(
+                new Config(
+                    new Config.DelayConfig.Builder().build(),
+                    new Config.FailureConfig.Builder()
+                            .setPercentage(100)
+                            .setExceptionClassName("java.io.IOException")
+                            .build(),
+                    true)));
+    int n = 1000;
+    for (int i = 0; i < n; i++) {
+      assertThrows(IOException.class, () -> interceptor.intercept(callable));
+    }
+    verify(callable, never()).call();
+  }
+}
