e993eca88e170502a6229739b60fd713c45f6d64
Mitsunori Komatsu <komamitsu@gmail.com>
2024-09-16 23:35:24 +0900

    Add comments and simply the code

--- example/src/main/java/org/komamitsu/Main.java
+++ example/src/main/java/org/komamitsu/Main.java
@@ -13,30 +13,43 @@ import java.util.concurrent.*;
 
 public class Main {
   public static void main(String[] args) throws ExecutionException, InterruptedException {
+    // Create an Account DB with Account A, who has an initial balance of 200.
     AccountDb accountDb = new AccountDb(Tuple.of("A", 200L));
 
+    // Create a JobQueue with a 4-second lease duration.
     JobQueue jobQueue = new JobQueue(Duration.ofSeconds(4));
+
+    // Enqueue a job that increments the balance of Account A by 50.
     jobQueue.enqueue(new Job(UUID.randomUUID().toString(), () -> accountDb.incrementBalance("A", 50L)));
 
     try (ExecutorService executorService = Executors.newCachedThreadPool()) {
-      List<Future<?>> futures = new ArrayList<>();
-      for (int i = 0; i < 10; i++) {
-        int workerIndex = i;
-        futures.add(
-            executorService.submit(
-                () -> {
-                  Optional<Job> jobOpt = jobQueue.dequeue("worker-" + workerIndex);
-                  jobOpt.ifPresent(job -> {
-                    job.task.run();
-                    jobQueue.finishJob(job);
-                  });
-                }));
-        TimeUnit.SECONDS.sleep(1);
-      }
-      for (Future<?> future : futures) {
-        future.get();
-      }
+      // Worker 1 dequeues the job and processes it.
+      Future<?> worker1Future = executorService.submit(
+          () -> {
+            Optional<Job> jobOpt = jobQueue.dequeue("worker-1");
+            jobOpt.ifPresent(job -> {
+              job.task.run();
+              jobQueue.finishJob(job);
+            });
+          });
 
+      // Wait for 5 seconds. Worker 1 should have finished the job by now, right?
+      TimeUnit.SECONDS.sleep(5);
+
+      // Worker 2 dequeues any available job and processes it if it exists.
+      Future<?> worker2Future = executorService.submit(
+          () -> {
+            Optional<Job> jobOpt = jobQueue.dequeue("worker-2");
+            jobOpt.ifPresent(job -> {
+              job.task.run();
+              jobQueue.finishJob(job);
+            });
+          });
+
+      worker1Future.get();
+      worker2Future.get();
+
+      // The balance should be 250.
       System.out.printf("The balance of Account A: %d\n", accountDb.getBalance("A"));
     }
   }
